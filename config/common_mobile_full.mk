f83235a21afc3347962c80cfb2d18bf2acd6ee0d
Pranav Vashi <neobuddy89@gmail.com>
2023-10-16 22:58:04 +0530

    vendor: Remove packages moved to addons
    Change-Id: I6af87bbb53245a407ca2566c0d708ef0da5f8b4c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,8 +3,6 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include GoogleSansFlex font
-$(call inherit-product-if-exists, external/google-fonts/google-sans-flex/fonts.mk)
 
 # Apps
 PRODUCT_PACKAGES += \
@@ -30,10 +28,6 @@ PRODUCT_PACKAGES += \
     unrar \
     zstd
 
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontGoogleSansFlexOverlay
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
