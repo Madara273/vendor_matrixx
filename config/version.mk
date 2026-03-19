ac85e9ab29251eefc28225f092b7d920bf9788ab
Pranav Vashi <neobuddy89@gmail.com>
2026-03-19 23:47:37 +0530

    build: Ship as ProjectMatrixx based on LOS * While LOS is base ROM, there are/will be additions over LOS.   Therefore, rebrand and ship it as ProjectMatrixx ROM.
    Change-Id: I53c407da455885af7029853ff635090f34b01f0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -1,45 +1,32 @@
-PRODUCT_VERSION_MAJOR = 23
-PRODUCT_VERSION_MINOR = 2
+PRODUCT_VERSION_MAJOR = 16
+PRODUCT_VERSION_MINOR = 0
 
-ifeq ($(LINEAGE_VERSION_APPEND_TIME_OF_DAY),true)
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d_%H%M%S)
+# Increase Lunaris Version with each major release.
+MATRIXX_VERSION := 12.2.0
+
+MATRIXX_RELEASE_CODE := Protium
+
+MATRIXX_BUILD_TYPE := $(shell vendor/lineage/build/verifykeys.sh $(DEFAULT_SYSTEM_DEV_CERTIFICATE))
+
+ifeq ($(WITH_GMS), true)
+  MATRIXX_BUILD_VARIANT := Gapps
 else
-    LINEAGE_BUILD_DATE := $(shell date -u +%Y%m%d)
+  MATRIXX_BUILD_VARIANT := Vanilla
 endif
-
-# Set LINEAGE_BUILDTYPE from the env RELEASE_TYPE, for jenkins compat
-
-ifndef LINEAGE_BUILDTYPE
-    ifdef RELEASE_TYPE
-        # Starting with "LINEAGE_" is optional
-        RELEASE_TYPE := $(shell echo $(RELEASE_TYPE) | sed -e 's|^LINEAGE_||g')
-        LINEAGE_BUILDTYPE := $(RELEASE_TYPE)
-    endif
-endif
-
-# Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter RELEASE NIGHTLY SNAPSHOT EXPERIMENTAL,$(LINEAGE_BUILDTYPE)),)
-    LINEAGE_BUILDTYPE := UNOFFICIAL
-    LINEAGE_EXTRAVERSION :=
-endif
-
-ifeq ($(LINEAGE_BUILDTYPE), UNOFFICIAL)
-    ifneq ($(TARGET_UNOFFICIAL_BUILD_ID),)
-        LINEAGE_EXTRAVERSION := -$(TARGET_UNOFFICIAL_BUILD_ID)
-    endif
-endif
-
-LINEAGE_VERSION_SUFFIX := $(LINEAGE_BUILD_DATE)-$(LINEAGE_BUILDTYPE)$(LINEAGE_EXTRAVERSION)-$(LINEAGE_BUILD)
 
 # Internal version
-LINEAGE_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_VERSION_SUFFIX)
+LINEAGE_VERSION := Matrixx$(MATRIXX_VARIANT)-v$(MATRIXX_VERSION)-$(MATRIXX_BUILD_TYPE)-$(LINEAGE_BUILD)-$(MATRIXX_BUILD_VARIANT)-$(shell date +%Y%m%d-%H%M)
 
 # Display version
-LINEAGE_DISPLAY_VERSION := $(PRODUCT_VERSION_MAJOR)-$(LINEAGE_VERSION_SUFFIX)
+LINEAGE_DISPLAY_VERSION := Matrixx-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(MATRIXX_VERSION)
 
 # LineageOS version properties
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE)
+    ro.matrixx.build.version=$(MATRIXX_VERSION) \
+    ro.matrixx.display.version=$(LINEAGE_DISPLAY_VERSION) \
+    ro.matrixx.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.matrixx.build.variant=$(MATRIXX_BUILD_VARIANT) \
+    ro.matrixx.release.type=$(MATRIXX_BUILD_TYPE) \
+    ro.matrixx.release.code=$(MATRIXX_RELEASE_CODE) \
+    ro.matrixx.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.modversion=$(MATRIXX_VERSION)
