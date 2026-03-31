2dc27c36c1a6c47e8e7a00d9952b88cf4eb0912c
Ghosuto <clash.raja10@gmail.com>
2026-03-31 16:20:47 +0000

    config: add surfaceflinger boost prop
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/matrixx.mk
+++ config/matrixx.mk
@@ -237,3 +237,8 @@ ifeq ($(PERF_ANIM_OVERRIDE),true)
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.predict_hwc_composition_strategy=0
 endif
+
+ifeq ($(SURFACE_FLINGER_BOOST),true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.uclamp.min=180
+endif
