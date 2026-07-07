0ea71d08fc0666f0b361cf0a0a81031f1e2ab6b0
aamirr-ali <amirwide@gmail.com>
2026-07-08 02:49:02 +0530

    version: Bump Version to->v12.5-Delta

--- config/version.mk
+++ config/version.mk
@@ -2,9 +2,9 @@ PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
 # Increase Matrixx Version with each major release.
-MATRIXX_BASE_VERSION := 12.4
+MATRIXX_BASE_VERSION := 12.5
 
-MATRIXX_CODENAME := Catalyst
+MATRIXX_CODENAME := Delta
 
 MATRIXX_BUILD_TYPE := $(shell vendor/matrixx/build/verifykeys.sh $(DEFAULT_SYSTEM_DEV_CERTIFICATE))
 
