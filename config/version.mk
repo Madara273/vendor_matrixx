9e8e24a70076e1bf39e97011133328c3fa6dbc7f
Madara273 <ravenhoxs@gmail.com>
2026-07-12 11:08:50 +0300

    matrixx vendor: version Bump Version to->v12.6-Expanse
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -2,9 +2,9 @@ PRODUCT_VERSION_MAJOR = 16
 PRODUCT_VERSION_MINOR = 0
 
 # Increase Matrixx Version with each major release.
-MATRIXX_BASE_VERSION := 12.5
+MATRIXX_BASE_VERSION := 12.6
 
-MATRIXX_CODENAME := Delta
+MATRIXX_CODENAME := Expanse
 
 MATRIXX_BUILD_TYPE := $(shell vendor/matrixx/build/verifykeys.sh $(DEFAULT_SYSTEM_DEV_CERTIFICATE))
 
